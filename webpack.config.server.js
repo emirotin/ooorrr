94fdf383bc9f0e80595a60fe3b38e9164f4aa80b
Eugene Mirotin <emirotin@gmail.com>
2017-08-10 13:20:46 +0300

    now depoyments

--- webpack.config.server.js
+++ webpack.config.server.js
@@ -3,36 +3,41 @@ const path = require('path')
 const nodeExternals = require('webpack-node-externals')
 const StartServerPlugin = require('start-server-webpack-plugin')
 
+const isProd = process.env.NODE_ENV === 'production'
+const isDev = !isProd
+
+const filter = a => a.filter(Boolean)
+
 module.exports = {
-    entry: [
-        'webpack/hot/poll?1000',
-        './server/index'
-    ],
-    watch: true,
-    target: 'node',
-    externals: [nodeExternals({
-        whitelist: ['webpack/hot/poll?1000']
-    })],
-    module: {
-        rules: [{
-            test: /\.js?$/,
-            use: 'babel-loader',
-            exclude: /node_modules/
-        }]
-    },
-    plugins: [
-        new StartServerPlugin('server.js'),
-        new webpack.NamedModulesPlugin(),
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.NoEmitOnErrorsPlugin(),
-        new webpack.DefinePlugin({
-            "process.env": {
-                "BUILD_TARGET": JSON.stringify('server')
-            }
-        }),
-    ],
-    output: {
-        path: path.join(__dirname, '.build'),
-        filename: 'server.js'
-    }
+  entry: filter([
+    isDev && 'webpack/hot/poll?1000',
+    './server/index'
+  ]),
+  watch: isDev,
+  target: 'node',
+  externals: [nodeExternals({
+    whitelist: isDev ? ['webpack/hot/poll?1000'] : undefined
+  })],
+  module: {
+    rules: [{
+      test: /\.js?$/,
+      use: 'babel-loader',
+      exclude: /node_modules/
+    }]
+  },
+  plugins: filter([
+    isDev && new StartServerPlugin('server.js'),
+    new webpack.NamedModulesPlugin(),
+    isDev && new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.DefinePlugin({
+      "process.env": {
+        "BUILD_TARGET": JSON.stringify('server')
+      }
+    }),
+  ]),
+  output: {
+    path: path.join(__dirname, '.build'),
+    filename: 'server.js'
+  }
 }
